cd7d032cd236928a93954279e23c8ef4b82d516a
Sherin Thomas <112325140+sherin17@users.noreply.github.com>
2023-12-12 18:53:41 +0000

    init

--- Static/JS/script.js
+++ Static/JS/script.js
@@ -24,7 +24,7 @@
 $('.addToCart').on("click", function (event) {
     if ($(this).prev().prev().prev().find("input").val() == '0') {
         event.preventDefault();
-        $(this).next().next().next().html("please select at least one book");
+        $(this).next().next().next().html("please select at least one item");
         $(this).next().next().next().css("display", "block");
         $(this).next().next().next().delay(3000).slideUp();
     }
